944dc65ba147ef9bfc9be0bf4c33fbd7099d1008
Jonas Offersen <72074478+zelanard@users.noreply.github.com>
2023-09-14 21:46:36 +0200

    Removed unnecesary Usings.

--- TheLibrary/Model/Book.cs
+++ TheLibrary/Model/Book.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TheLibrary.Model
+﻿namespace TheLibrary.Model
 {
     /// <summary>
     /// <c>Book</c> Models a book.
